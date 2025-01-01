75b16c988c97866db00209f0c67b2448f0930977
jasonhan3 <jh146@rice.edu>
2025-01-01 13:51:15 -0600

    added run-gpu.sh

--- run-gpu.sh
+++ run-gpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pip install flwr==1.5.0 tqdm numpy pennylane "ray>=2.3.0" matplotlib pillow scikit-learn seaborn pandas opacus pyyaml tenseal kaggle torch torchvision torchaudio
+TMPDIR=/home/jh146/ptl_rdf/QFL-MLNCP-NeurIPS/tmpdir pip install --cache-dir=/home/jh146/ptl_rdf/QFL-MLNCP-NeurIPS/tmpdir flwr==1.5.0 tqdm numpy==1.26.4 pennylane "ray>=2.3.0" matplotlib pillow scikit-learn seaborn pandas opacus pyyaml tenseal kaggle torch==2.1.2 torchvision==0.16.2 torchaudio
